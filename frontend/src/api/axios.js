6e12b47e9449bba7bb269f7676786abd856d173e
Misbah <misbahnoor3252@gmail.com>
2026-03-05 15:38:53 +0500

    fix axios path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 
-    "https://mini-ecommerce-command-center-production-03ee.up.railway.app/api",    
+    "https://mini-ecommerce-command-center-production-03ee.up.railway.app",    
     withCredentials: true,
 });
 
