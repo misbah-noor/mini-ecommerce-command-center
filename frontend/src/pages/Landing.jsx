fc8d9478ce4d96c3f791a6e19b7e7f8cd3e89f52
Misbah <misbahnoor3252@gmail.com>
2026-03-09 05:34:04 +0500

    landing page added

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -11,27 +11,9 @@ const Landing = () => {
       <div className="absolute top-0 left-0 w-96 h-96 bg-[var(--color-primary)]/20 blur-3xl rounded-full"></div>
       <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-[var(--color-primary)]/10 blur-3xl rounded-full"></div>
 
-      {/* ---------------- NAVBAR ---------------- */}
-      <nav className="flex justify-between px-16 py-4 items-center px-8 py-5 border-b border-gray-200">
-
-        {/* Logo */}
-        <div className="flex items-center gap-3">
-            <img src="/L2.png" alt="logo" className="w-10 h-10" />
-          <span className="text-2xl font-sans font-bold text-[var(--color-primary)]">Mini E-Store</span>
-        </div>
-
-        {/* Login Button */}
-        <button
-          onClick={() => navigate("/login")}
-          className="px-8 py-3 bg-[var(--color-primary)] text-white text-lg cursor-pointer rounded-lg font-semibold hover:opacity-90"
-        >
-          Login
-        </button>
-
-      </nav>
-
+   
       {/* ---------------- HERO SECTION ---------------- */}
-      <div className="flex flex-col items-center justify-center flex-1 text-center px-6">
+      <div className="flex flex-col pt-16 items-center justify-center flex-1 text-center px-6">
 
         {/* Image */}
         <motion.img
